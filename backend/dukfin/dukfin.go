e0b173149efc403e4c9d0facb4914a72446b9621
Robert Schumann <dukfaar@gmail.com>
2022-03-23 13:02:27 +0100

    generate initial stuff

---
+++ backend/dukfin/dukfin.go
@@ -0,0 +1,7 @@
+package dukfin
+
+import "github.com/dukfaar/dukfin/shared/dukfin"
+
+type DefaultDukFinServer struct {
+	shared.UnimplementedDukFinServer
+}
--- backend/server.go
+++ backend/server.go
@@ -5,25 +5,11 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/dukfaar/dukfin/backend/graph"
-	"github.com/dukfaar/dukfin/backend/graph/generated"
+	"github.com/dukfaar/dukfin/backend/dukfin"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	DefaultDukFinServer
 }
